b87711b9201758a179684eb8feba80faa4f3f4db
ddfPig <ddf-128@163.com>
2020-01-18 10:20:50 +0800

    123

---
+++ src/Tools.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/1/18
+ * Time: 10:00
+ */
+namespace tools;
+class Tools
+{
+    public function say()
+    {
+        echo "开发包测试";
+    }
+}
